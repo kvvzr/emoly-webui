3e2b5a69719b52d62e5e8c12db18cb11ddde00ec
kvvzr <kwzr@twinkrun.net>
2016-03-29 23:44:23 +0900

    remove job queue

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import config
-from celery import Celery, task
+# from celery import Celery, task
 from flask import Flask, render_template, request
 from gensim.models.word2vec import Word2Vec
 from gensim import matutils
@@ -12,25 +12,25 @@ app.debug = True
 app.config.update(
     TEMPLATE_FOLDER=config.dirs['template'],
     STATIC_FOLDER=config.dirs['static'],
-    CELERY_BROKER_URL=config.celery_broker_url,
-    CELERY_RESULT_BACKEND=config.celery_result_url
+    # CELERY_BROKER_URL=config.celery_broker_url,
+    # CELERY_RESULT_BACKEND=config.celery_result_url
 )
 
 mecab = MeCab.Tagger('-d %s' % (config.mecab_dic_dir))
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-celery = make_celery(app)
+# def make_celery(app):
+#     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+#     celery.conf.update(app.config)
+#     TaskBase = celery.Task
+#     class ContextTask(TaskBase):
+#         abstract = True
+#         def __call__(self, *args, **kwargs):
+#             with app.app_context():
+#                 return TaskBase.__call__(self, *args, **kwargs)
+#     celery.Task = ContextTask
+#     return celery
+#
+# celery = make_celery(app)
 
 all_model = Word2Vec.load_word2vec_format('model/all.txt', binary=False, unicode_errors='ignore')
 emoji_model = Word2Vec.load_word2vec_format('model/emoji.txt', binary=False, unicode_errors='ignore')
@@ -41,7 +41,7 @@ def most_sim_vec(model, word=[]):
     best = matutils.argsort(dists, topn=1, reverse=True)
     return model.index2word[best[0]]
 
-@task(name='tasks.emolize')
+# @task(name='tasks.emolize')
 def emolize(text):
     result = ''
     for line in text.split('\n'):
@@ -68,17 +68,18 @@ def index():
 @app.route('/emoly', methods=['POST'])
 def emoly():
     text = request.form['text']
-    result = emolize.delay(text)
-    return str(result)
+    # result = emolize.delay(text)
+    # return str(result)
+    return str(emolize(text))
 
-@app.route('/result/<job_key>', methods=['GET'])
-def result(job_key):
-    job = emolize.AsyncResult(job_key)
-    ready = job.ready()
-    if (ready):
-        return str(job.get())
-    else:
-        return ('', 202)
+# @app.route('/result/<job_key>', methods=['GET'])
+# def result(job_key):
+#     job = emolize.AsyncResult(job_key)
+#     ready = job.ready()
+#     if (ready):
+#         return str(job.get())
+#     else:
+#         return ('', 202)
 
 if __name__ == '__main__':
     app.run()
--- static/index.js
+++ static/index.js
@@ -3,29 +3,36 @@
 
     $('#result-container').hide();
 
-    var checkResult = function(jobId) {
-        $.get('/result/' + jobId).done(function(res, status, xhr) {
-            if (xhr.status === 202) {
-                window.setTimeout(function() {
-                    checkResult(jobId);
-                }, 500);
-            } else if (xhr.status == 200) {
-                $('#result-text').show();
-                $('#result-text').html(res.replace(/\n/g,'<br/>'));
-                $('#progressbar').hide();
-
-                var url = "https://twitter.com/intent/tweet?text=" + res + ' #emoly';
-                $("#share-container").empty().append( '<a class="twitter-share-button" href="' + url + '" data-size="large"> Tweet</a>' );
-                twttr.widgets.load();
-            }
-        });
-    };
+    // var checkResult = function(jobId) {
+    //     $.get('/result/' + jobId).done(function(res, status, xhr) {
+    //         if (xhr.status === 202) {
+    //             window.setTimeout(function() {
+    //                 checkResult(jobId);
+    //             }, 500);
+    //         } else if (xhr.status == 200) {
+    //             $('#result-text').show();
+    //             $('#result-text').html(res.replace(/\n/g,'<br/>'));
+    //             $('#progressbar').hide();
+    //
+    //             var url = "https://twitter.com/intent/tweet?text=" + res + ' #emoly';
+    //             $("#share-container").empty().append( '<a class="twitter-share-button" href="' + url + '" data-size="large"> Tweet</a>' );
+    //             twttr.widgets.load();
+    //         }
+    //     });
+    // };
 
     var requestEmoly = function(data) {
         $.post('/emoly', data).done(function(res) {
-            window.setTimeout(function() {
-                checkResult(res);
-            }, 500);
+            $('#result-text').show();
+            $('#result-text').html(res.replace(/\n/g,'<br/>'));
+            $('#progressbar').hide();
+
+            var url = "https://twitter.com/intent/tweet?text=" + res + ' #emoly';
+            $("#share-container").empty().append( '<a class="twitter-share-button" href="' + url + '" data-size="large"> Tweet</a>' );
+            twttr.widgets.load();
+            // window.setTimeout(function() {
+            //     checkResult(res);
+            // }, 500);
         });
     };
 
